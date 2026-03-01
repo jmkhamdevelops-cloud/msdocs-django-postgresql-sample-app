f84bad63f2691eae18bc3d272a77fa147514a951
jmkhamdevelops-cloud <jmkhamdevelops@gmail.com>
2026-03-01 16:40:32 -0600

    initial commit

--- azureproject/production.py
+++ azureproject/production.py
@@ -26,6 +26,7 @@ SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+<<<<<<< HEAD
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -46,3 +47,27 @@ CACHES = {
         },
     }
 }
+=======
+# Configure Postgres database based on connection string of the libpq Keyword/Value form
+# https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
+# Uncomment the following lines for App Service
+# conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+# conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+# DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+#     dbuser=conn_str_params['user'],
+#     dbpass=conn_str_params['password'],
+#     dbhost=conn_str_params['host'],
+#     dbname=conn_str_params['dbname']
+# )
+
+# CACHES = {
+#         "default": {  
+#             "BACKEND": "django_redis.cache.RedisCache",
+#             "LOCATION": os.environ.get('AZURE_REDIS_CONNECTIONSTRING'),
+#             "OPTIONS": {
+#                 "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#                 "COMPRESSOR": "django_redis.compressors.zlib.ZlibCompressor",
+#         },
+#     }
+# }
+>>>>>>> e4ea5e5 (remove infra files)
