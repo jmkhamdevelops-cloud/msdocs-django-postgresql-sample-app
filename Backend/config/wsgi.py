72dddb4c1e581e2283151125fd726e16e03a0477
jmkhamdevelops-cloud <jmkhamdevelops@gmail.com>
2026-03-01 21:10:17 -0600

    Changed wsgi.py

--- Backend/config/wsgi.py
+++ Backend/config/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 # Check for the WEBSITE_HOSTNAME environment variable to see if we are running in Azure Ap Service
 # If so, then load the settings from production.py
-settings_module = 'config.production' if 'WEBSITE_HOSTNAME' in os.environ else 'config.settings'
+settings_module = 'Backend/config.production' if 'WEBSITE_HOSTNAME' in os.environ else 'Backend/config.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
